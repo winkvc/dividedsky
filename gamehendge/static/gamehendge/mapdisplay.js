e12104f40a98f5c923a50979006a4a0de2127f76
Mark Miller <725mrm@gmail.com>
2017-09-15 20:17:16 -0700

    better requirements.txt

--- gamehendge/static/gamehendge/mapdisplay.js
+++ gamehendge/static/gamehendge/mapdisplay.js
@@ -1,8 +1,6 @@
 function initMap() {
 
   $.getJSON('station_locations/', function(myJsonObject) {
-    //var myJson = '{"data": [{"position": {"lat": 37.424261, "lng": -122.200397}, "icon": "https://img.pokemondb.net/sprites/ruby-sapphire/normal/ditto.png"}, {"position": {"lat": 37.422808, "lng": -122.19906}, "icon": "https://img.pokemondb.net/sprites/ruby-sapphire/normal/ditto.png"}]}';
-    //var myJsonObject = response
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 14,
       center: myJsonObject.data[0].position
