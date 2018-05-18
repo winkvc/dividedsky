7f232a0feae476f544889a34626177e2fb9ec085
Mark Miller <725mrm@gmail.com>
2018-05-17 22:33:12 -0700

    hacky scrolling but it works

---
+++ unity/Lawn Boy/Assets/Scripts/DragAndScale.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndScale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("Clicked!");
+	}
+}
